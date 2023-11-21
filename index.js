ea51da684828b266bf06d3427b60401a7974763c
crackie-hakz <145805767+crackie-hakz@users.noreply.github.com>
2023-11-21 10:42:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const { ApolloServer } = require("apollo-server");
+const { importSchema } = require("graphql-import");
+const EtherDataSource = require("./datasource/ethDatasource");
+const typeDefs = importSchema("./schema.graphql");
+
+require("dotenv").config();
+
+const resolvers = {
+  Query: {
+    // Query to get ether balance by address
+    etherBalanceByAddress: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.etherBalanceByAddress(),
+
+    // Query to get total supply of ether
+    totalSupplyOfEther: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.totalSupplyOfEther(),
+
+    // Query to get latest ethereum price
+    latestEthereumPrice: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    // Query to get block confirmation time
+    blockConfirmationTime: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getBlockConfirmationTime(),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    ethDataSource: new EtherDataSource(),
+  }),
+});
+
+server.timeout = 0;
+server.listen("9000").then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+});
